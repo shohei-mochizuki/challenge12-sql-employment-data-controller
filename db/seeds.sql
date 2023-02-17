78623be3c86d07100ab9198978b368d1ef71d677
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-02-17 13:08:25 -0500

    Updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,20 @@
-INSERT INTO book_prices (price)
+INSERT INTO department (department_name)
 VALUES (1),
        (2),
        (5),
        (10),
        (15);
 
-INSERT INTO favorite_books (book_name, in_stock, book_price)
+INSERT INTO role (role_title, role_salary, department_id)
+VALUES ("The Great Gatsby", true, 1),
+       ("Huckleberry Finn", true, 3),
+       ("100 Years of Solitude", false, 5),
+       ("Things Fall Apart", false, 1),
+       ("Crime and Punishment", true, 2),
+       ("Moby Dick", true, 4),
+       ("Decameron", false, 1);
+
+INSERT INTO employee (role_title, role_salary, department_id)
 VALUES ("The Great Gatsby", true, 1),
        ("Huckleberry Finn", true, 3),
        ("100 Years of Solitude", false, 5),
